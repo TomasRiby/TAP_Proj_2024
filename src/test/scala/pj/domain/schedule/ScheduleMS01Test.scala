83de8cb3bb1f4f646aaec23f6401e53936e8dc37
Tomás Ribeiro <1191113@isep.ipp.pt>
2024-03-27 16:28:02 +0000

    Trying to figure out how to load the XML files

--- src/test/scala/pj/domain/schedule/ScheduleMS01Test.scala
+++ src/test/scala/pj/domain/schedule/ScheduleMS01Test.scala
@@ -8,22 +8,16 @@ import pj.xml.XML
 // TODO: Create the code to test a functional domain model for schedule creation.
 //       create files in the files/test/ms01 folder
 class ScheduleMS01Test extends AnyFunSuite:
-//Isto é so random
-  test("fromNode should return correct node if it exists"):
+  //Isto é so random
+  test("Just testing things"):
+    //loads the file
     val result = FileIO.load("files/test/ms01/simple01.xml")
     result match {
+      //if successful
       case Right(xml) =>
-        val resources = (xml \\ "resources").headOption
-        resources match
-          case Some(xml) =>
-            val teachers = (xml \\ "teachers").headOption
-            teachers match
-              case Some(xml) =>
-                println(xml)
-              case None =>
-                println("NO TEACHERS")
-          case None =>
-            println("No <viva> element found in the XML.")
+        //load resources
+        val resources = XML.fromNode(xml, "resources")
+        print(resources)
       case Left(error) =>
         println(s"Error loading XML file: $error")
     }
