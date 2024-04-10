b00be80f9f6cc2dbe0bb1dbbdc0d9e9a63de2359
tomasoliveira21 <tomasoliveira1018@gmail.com>
2024-04-10 22:34:49 +0100

    opaque types

---
+++ src/main/scala/pj/typeUtils/opaqueTypes/opaqueTypes.scala
@@ -0,0 +1,21 @@
+package pj.typeUtils.opaqueTypes
+
+object opaqueTypes {
+  opaque type ID = String
+  opaque type Name = String
+  opaque type Start = String
+  opaque type End = String
+  opaque type Preference = Int
+
+  object ID {
+    def apply(value: String): ID = value
+  }
+
+  object Name {
+    def apply(value: String): Name = value
+  }
+
+  object Start {
+    def apply(value: String): Start = value
+  }
+}
