77086aadf95be75ba4a7cedaeea94db3fbd1dc59
Bruno Santos <bruno.santos@five9.com>
2024-04-17 22:10:17 +0100

    Add tests for AgendaIO invalid_external_Name_in,invalid_externalDuplicate_in, invalid_student_in,invalid_teacher_Id_in

--- src/test/scala/pj/io/AgendaIOTest.scala
+++ src/test/scala/pj/io/AgendaIOTest.scala
@@ -11,6 +11,10 @@ class AgendaIOTest extends AnyFunSuite:
 
   val invalid_Availability_in = "files/test/ms01/invalid_Availability_in.xml"
   val invalid_external_Id_in = "files/test/ms01/invalid_Availability_in.xml"
+  val invalid_external_Name_in = "files/test/ms01/invalid_external_Name_in.xml"
+  val invalid_externalDuplicate_in = "files/test/ms01/invalid_external_Name_in.xml"
+  val invalid_student_in = "files/test/ms01/invalid_external_Name_in.xml"
+  val invalid_teacher_Id_in = "files/test/ms01/invalid_external_Name_in.xml"
 
   test("loadAgenda returns Left for invalid_Availability_in"):
     val result = AgendaIO.loadAgenda(invalid_Availability_in)
@@ -25,3 +29,31 @@ class AgendaIOTest extends AnyFunSuite:
       result match
         case Right(agenda) => fail("The externalID format is wrong.")
         case Left(error) => succeed
+
+  test("loadAgenda returns Left for invalid_external_Name_in"):
+    val result = AgendaIO.loadAgenda(invalid_external_Name_in)
+
+    result match
+      case Right(agenda) => fail("The externalID format is wrong.")
+      case Left(error) => succeed
+
+  test("loadAgenda returns Left for invalid_externalDuplicate_in"):
+    val result = AgendaIO.loadAgenda(invalid_externalDuplicate_in)
+
+    result match
+      case Right(agenda) => fail("The externalID format is wrong.")
+      case Left(error) => succeed
+
+  test("loadAgenda returns Left for invalid_student_in"):
+    val result = AgendaIO.loadAgenda(invalid_student_in)
+
+    result match
+      case Right(agenda) => fail("The externalID format is wrong.")
+      case Left(error) => succeed
+
+  test("loadAgenda returns Left for invalid_teacher_Id_in"):
+    val result = AgendaIO.loadAgenda(invalid_teacher_Id_in)
+
+    result match
+      case Right(agenda) => fail("The externalID format is wrong.")
+      case Left(error) => succeed
