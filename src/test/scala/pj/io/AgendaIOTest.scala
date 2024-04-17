cc2ac94746237ac527daa85affb38da5cdc82563
Bruno Santos <bruno.santos@five9.com>
2024-04-17 22:06:28 +0100

    Add tests for AgendaIO invalid_external_Id_in

--- src/test/scala/pj/io/AgendaIOTest.scala
+++ src/test/scala/pj/io/AgendaIOTest.scala
@@ -10,10 +10,18 @@ import pj.domain.{Agenda, Result}
 class AgendaIOTest extends AnyFunSuite:
 
   val invalid_Availability_in = "files/test/ms01/invalid_Availability_in.xml"
+  val invalid_external_Id_in = "files/test/ms01/invalid_Availability_in.xml"
 
-  test("loadAgenda deve retornar uma agenda válida para um XML válido"):
+  test("loadAgenda returns Left for invalid_Availability_in"):
     val result = AgendaIO.loadAgenda(invalid_Availability_in)
 
     result match
       case Right(agenda) => fail("The availability format is wrong.")
       case Left(error) => succeed
+
+  test("loadAgenda returns Left for invalid_external_Id_in"):
+      val result = AgendaIO.loadAgenda(invalid_Availability_in)
+
+      result match
+        case Right(agenda) => fail("The externalID format is wrong.")
+        case Left(error) => succeed
