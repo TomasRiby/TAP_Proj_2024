3eca94fbd5d2bc6aca29f2286e749ecd990d2102
tomasoliveira21 <tomasoliveira1018@gmail.com>
2024-05-18 20:57:42 +0100

    feat: makeNonOverlappingAvailabilities property tests

--- src/test/scala/pj/MS02/domain/AvailabilityTest.scala
+++ src/test/scala/pj/MS02/domain/AvailabilityTest.scala
@@ -6,9 +6,17 @@ import pj.MS02.opaque.OTimeTest.generateTime
 import pj.MS02.opaque.PreferenceTest.generatePreference
 import pj.domain.Availability
 import pj.opaqueTypes.ID.ID
+import pj.opaqueTypes.OTime.OTime
 
 object AvailabilityTest
   extends Properties("Availability Test"):
+
+  def generateNonOverlappingAvailabilityList: Gen[List[Availability]] =
+    for {
+      listLength <- Gen.choose(1, 20)
+      availabilities <- Gen.listOfN(listLength, generateAvailability)
+    } yield makeNonOverlapping(availabilities)
+
   def generateAvailability: Gen[Availability] =
     for {
       start <- generateTime
@@ -20,14 +28,27 @@ object AvailabilityTest
         case Right(valid) => Gen.const(valid)
     } yield validAvailability
 
+  def makeNonOverlapping(availabilities: List[Availability]): List[Availability] =
+    availabilities.sortBy(_.start).foldLeft(List.empty[Availability]) { (acc, avail) =>
+      acc match
+        case Nil => List(avail)
+        case head :: _ =>
+          if (avail.start.isAfter(head.end) || avail.start.isEqual(head.end)) avail :: acc
+          else acc
+    }.reverse
+
+  property("Testing Non-Overlapping Availability List") = forAll(generateNonOverlappingAvailabilityList) { list =>
+    list.zip(list.drop(1)).forall { case (a, b) => b.start.isAfter(a.end) || b.start.isEqual(a.end) }
+  }
+
+  property("Testing Availability") = forAll(generateAvailability) { avail =>
+    avail.isValid
+  }
+
+  property("Testing Availability List") = forAll(generateAvailabilityList) { list =>
+    list.forall(_.isValid)
+  }
+
+  // Ensuring existing tests use the new non-overlapping list generator
   def generateAvailabilityList: Gen[List[Availability]] =
-    for {
-      listLenght <- Gen.choose(1, 20)
-      availabilityList <- Gen.listOfN(listLenght, generateAvailability)
-    } yield availabilityList
-
-  property("Testing Availability") = forAll(generateAvailability):
-    _.isValid
-
-  property("Testing Availability List") = forAll(generateAvailabilityList):
-    _.forall(_.isValid)
+    generateNonOverlappingAvailabilityList
