206d2a659ff99559b3426f5bf79528fccc69b32f
Tomás Ribeiro <1191113@isep.ipp.pt>
2024-05-18 23:43:13 +0100

    Each Viva is Unique

--- src/test/scala/pj/MS02/domain/VivaTest.scala
+++ src/test/scala/pj/MS02/domain/VivaTest.scala
@@ -3,41 +3,91 @@ package pj.MS02.domain
 import org.scalacheck.Prop.{AnyOperators, forAll}
 import org.scalacheck.{Gen, Properties}
 import pj.MS02.domain.AvailabilityTest.generateAvailabilityList
-import pj.MS02.opaque.IDTest.{generateExternalID, generateID, generateTeacherID, generateUniqueIDs}
+import pj.MS02.opaque.IDTest.{generateExternalID, generateExternalIdThatsNotOnList, generateID, generateTeacherID, generateTeacherIdThatsNotOnList, generateUniqueIDs}
 import pj.MS02.opaque.NameTest.generateName
 import pj.domain.{Advisor, CoAdvisor, External, President, Supervisor, Teacher, Viva}
 import pj.opaqueTypes.ID
 import pj.opaqueTypes.ID.{ID, createRegularId}
 
-object VivaTest
-  extends Properties("Testing Viva"):
 
-  def generatePresident(ids: List[ID]): Gen[(President, List[ID])] =
-    for
-      chosenId <- Gen.oneOf(ids.filter(_.isTeacherId))
-    yield (President.from(chosenId), ids.filterNot(_ == chosenId))
+object VivaTest extends Properties("Testing Viva"):
 
-  def generateAdvisor(ids: List[ID]): Gen[(Advisor, List[ID])] =
-    for
-      chosenId <- Gen.oneOf(ids.filter(_.isTeacherId))
-    yield (Advisor.from(chosenId), ids.filterNot(_ == chosenId))
+  def generatePresident(ids: List[ID]): Gen[(President, List[ID])] = for {
+    chosenId <- generateTeacherIdThatsNotOnList(ids)
+  } yield (President.from(chosenId), chosenId :: ids)
 
-  def generateViva(idList: List[ID]): Gen[Viva] =
-    for
+  def generateAdvisor(ids: List[ID]): Gen[(Advisor, List[ID])] = for {
+    chosenId <- generateTeacherIdThatsNotOnList(ids)
+  } yield (Advisor.from(chosenId), chosenId :: ids)
+
+  def generateCoAdvisor(ids: List[ID]): Gen[(CoAdvisor, List[ID])] = for {
+    chosenId <- Gen.oneOf(generateTeacherIdThatsNotOnList(ids), generateExternalIdThatsNotOnList(ids))
+  } yield (CoAdvisor.from(chosenId), chosenId :: ids)
+
+  def generateSupervisor(ids: List[ID]): Gen[(Supervisor, List[ID])] = for {
+    chosenId <- generateExternalIdThatsNotOnList(ids)
+  } yield (Supervisor.from(chosenId), chosenId :: ids)
+
+  def generateSupervisorList(ids: List[ID]): Gen[(List[Supervisor], List[ID])] =
+    def helper(currentIds: List[ID], remaining: Int, acc: List[Supervisor]): Gen[(List[Supervisor], List[ID])] =
+      if (remaining <= 0) Gen.const((acc, currentIds))
+      else
+        generateSupervisor(currentIds).flatMap { case (supervisor, updatedIds) =>
+          helper(updatedIds, remaining - 1, supervisor :: acc)
+        }
+
+    for {
+      count <- Gen.choose(0, 5)
+      result <- helper(ids, count, List.empty)
+    } yield result
+
+  // Property to test CoAdvisors
+  def generateCoAdvisorList(ids: List[ID]): Gen[(List[CoAdvisor], List[ID])] =
+    def helper(currentIds: List[ID], remaining: Int, acc: List[CoAdvisor]): Gen[(List[CoAdvisor], List[ID])] =
+      if (remaining <= 0) Gen.const((acc, currentIds))
+      else
+        generateCoAdvisor(currentIds).flatMap { case (coAdvisor, updatedIds) =>
+          helper(updatedIds, remaining - 1, coAdvisor :: acc)
+        }
+
+    for {
+      count <- Gen.choose(0, 5)
+      result <- helper(ids, count, List.empty)
+    } yield result
+
+  def generateViva: Gen[Viva] =
+    val inicialList = List.empty
+    for {
       student <- generateName
       title <- generateName
-      (president, remainingIds) <- generatePresident(idList)
-      (advisor, updatedIds) <- generateAdvisor(remainingIds)
-    yield Viva.from(student, title, president, advisor, List.empty, List.empty)
+      (president, idsWithPresident) <- generatePresident(inicialList)
+      (advisor, idsWithAdvisor) <- generateAdvisor(idsWithPresident)
+      (supervisorList, idsWithSupervisors) <- generateSupervisorList(idsWithAdvisor)
+      (coAdvisorList, _) <- generateCoAdvisorList(idsWithSupervisors)
+    } yield Viva.from(student, title, president, advisor, supervisorList, coAdvisorList)
 
   def generateVivaList(size: Int): Gen[List[Viva]] = for {
-    initialIds <- generateUniqueIDs(size * 2) // Ensure enough unique IDs for all Viva
-    vivaList <- Gen.listOfN(size, generateViva(initialIds))
+    vivaList <- Gen.listOfN(size, generateViva)
   } yield vivaList
 
-  // Property to test Viva list generation
-  property("Each Viva in List doesnt have the same President and Advisor") = forAll(generateVivaList(50)) { vivaList =>
-    // Ensure all Viva instances have unique Presidents and Advisors
+
+  property("Each Viva in List doesn't have the same President and Advisor") = forAll(generateVivaList(50)) { vivaList =>
     vivaList.forall(viva => viva.president.id != viva.advisor.id)
   }
+
+  property("No Supervisors have the same ID within a Viva") = forAll(generateVivaList(50)) { vivaList =>
+    vivaList.forall(viva => viva.supervisor.map(_.id).distinct.sizeIs == viva.supervisor.size)
+  }
+
+  property("No CoAdvisors have the same ID within a Viva") = forAll(generateVivaList(50)) { vivaList =>
+    vivaList.forall(viva => viva.coAdvisor.map(_.id).distinct.sizeIs == viva.coAdvisor.size)
+  }
+
+  property("Each Viva has unique IDs across President, Advisor, Supervisors, and CoAdvisors") = forAll(generateVivaList(50)) { vivaList =>
+    vivaList.forall { viva =>
+      val allIds = viva.president.id :: viva.advisor.id :: viva.supervisor.map(_.id) ++ viva.coAdvisor.map(_.id)
+      allIds.distinct.sizeIs == allIds.size
+    }
+  }
+    
 
--- src/test/scala/pj/MS02/opaque/IDTest.scala
+++ src/test/scala/pj/MS02/opaque/IDTest.scala
@@ -40,10 +40,19 @@ object IDTest extends Properties("ID"):
 
   def generateUniqueIDs(maxNumber: Int): Gen[List[ID]] =
     for {
-      teacherIds <- Gen.listOfN(maxNumber/2, generateTeacherID)
-      externalIds <- Gen.listOfN(maxNumber/2, generateExternalID)
+      teacherIds <- Gen.listOfN(maxNumber / 2, generateTeacherID)
+      externalIds <- Gen.listOfN(maxNumber / 2, generateExternalID)
       res = teacherIds.distinct ++ externalIds.distinct
     } yield res
+
+  def generateTeacherIdThatsNotOnList(ids: List[ID]): Gen[ID] =
+    generateTeacherID.retryUntil(id => !ids.contains(id))
+
+  def generateExternalIdThatsNotOnList(ids: List[ID]): Gen[ID] =
+    generateExternalID.retryUntil(id => !ids.contains(id))
+
+  def generateIDThatsNotOnList(ids: List[ID]): Gen[ID] =
+    generateID.retryUntil(id => !ids.contains(id))
 
   property("IDs are in the correct format") = forAll(generateID):
     _.isValid
@@ -65,3 +74,6 @@ object IDTest extends Properties("ID"):
   property("External IDs are unique") = forAll(generateUniqueIDs(20)) { ids =>
     ids.filter(_.isExternalId).distinct.sizeIs == ids.count(_.isExternalId)
   }
+
+
+
