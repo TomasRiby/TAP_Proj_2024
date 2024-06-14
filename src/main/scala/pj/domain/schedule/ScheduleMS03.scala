a8ec83de10a7dac23d773d2460ed445eed387034
Tomás Ribeiro <1191113@isep.ipp.pt>
2024-06-14 14:04:37 +0100

    feat: added the MS03 files

--- src/main/scala/pj/domain/schedule/ScheduleMS03.scala
+++ src/main/scala/pj/domain/schedule/ScheduleMS03.scala
@@ -2,11 +2,19 @@ package pj.domain.schedule
 
 import scala.xml.Elem
 import pj.domain.*
+import pj.io.{AgendaIO, ScheduleIO}
 
 
 object ScheduleMS03 extends Schedule:
 
   // TODO: Create the code to implement a functional domain model for schedule creation
   //       Use the xml.XML code to handle the xml elements
-  //       Refer to https://github.com/scala/scala-xml/wiki/XML-Processing for xml creation  
-  def create(xml: Elem): Result[Elem] = ???
+  //       Refer to https://github.com/scala/scala-xml/wiki/XML-Processing for xml creation
+
+  def create(xml: Elem): Result[Elem] =
+    for {
+      agenda <- AgendaIO.loadAgenda(xml)
+      algorithm <- AlgorithmMS03.MS03_Algorithm(agenda)
+      res <- ScheduleIO.createScheduleXML(algorithm)
+    } yield res
+    
