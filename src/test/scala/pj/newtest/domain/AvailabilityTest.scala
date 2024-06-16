665bdb40b453daa63564f4ca0e93a96797a879d0
Bruno Santos <bruno.santos@five9.com>
2024-06-16 22:37:00 +0100

    Adding extra tests for availability

--- src/test/scala/pj/newtest/domain/AvailabilityTest.scala
+++ src/test/scala/pj/newtest/domain/AvailabilityTest.scala
@@ -2,27 +2,103 @@ package pj.newtest.domain
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
-import pj.domain.{Availability, DomainError}
-import pj.opaqueTypes.{OTime, Preference, ODuration}
+import org.scalatest.OptionValues
+import pj.domain.{Availability, DomainError, PreViva, RoleLinkedWithResource, Teacher, External, President, Advisor, Supervisor, CoAdvisor}
+import pj.opaqueTypes.{OTime, Preference, ODuration, ID, Name}
 import pj.opaqueTypes.OTime.OTime
 import pj.opaqueTypes.Preference.Preference
 import pj.opaqueTypes.ODuration.ODuration
+import pj.opaqueTypes.ID.ID
+import pj.opaqueTypes.Name.Name
+
 import java.time.LocalDateTime
+import scala.collection.immutable.HashSet
 
-class AvailabilityTest extends AnyFlatSpec with Matchers:
+class AvailabilityTest extends AnyFlatSpec with Matchers with OptionValues:
 
-  "from" should "create an Availability with valid OTime, OTime, and Preference" in:
-    val start = OTime.createTime("2023-06-14T10:15:30").getOrElse(fail("Failed to create OTime"))
-    val end = OTime.createTime("2023-06-14T12:15:30").getOrElse(fail("Failed to create OTime"))
-    val preference = Preference.createPreference(3).getOrElse(fail("Failed to create Preference"))
+  def createDuration(duration: String): ODuration =
+    ODuration.createDuration(duration).getOrElse(fail("Failed to create ODuration"))
 
-    val availability = Availability.from(start, end, preference)
+  def createValidPreViva(studentName: String, title: String, presidentId: String, advisorId: String, supervisorId: String, coAdvisorId: String): PreViva =
+    val student = Name.createName(studentName).getOrElse(fail("Failed to create Name"))
+    val thesisTitle = Name.createName(title).getOrElse(fail("Failed to create Name"))
+    val president = President.from(ID.createTeacherId(presidentId).getOrElse(fail("Failed to create ID")))
+    val advisor = Advisor.from(ID.createTeacherId(advisorId).getOrElse(fail("Failed to create ID")))
+    val supervisor = Supervisor.from(ID.createExternalId(supervisorId).getOrElse(fail("Failed to create External ID")))
+    val coAdvisor = CoAdvisor.from(ID.createTeacherId(coAdvisorId).getOrElse(fail("Failed to create ID")))
 
-    availability.start should be(start)
-    availability.end should be(end)
-    availability.preference should be(preference)
+    val teacher1Availability = List(
+      Availability.from(
+        OTime.createTime("2024-05-30T09:30:00").getOrElse(fail("Failed to create OTime")),
+        OTime.createTime("2024-05-30T12:30:00").getOrElse(fail("Failed to create OTime")),
+        Preference.createPreference(5).getOrElse(fail("Failed to create Preference"))
+      ),
+      Availability.from(
+        OTime.createTime("2024-05-30T13:30:00").getOrElse(fail("Failed to create OTime")),
+        OTime.createTime("2024-05-30T16:30:00").getOrElse(fail("Failed to create OTime")),
+        Preference.createPreference(3).getOrElse(fail("Failed to create Preference"))
+      )
+    )
 
-  "fromCheck" should "return Right for valid start and end times" in:
+    val teacher2Availability = List(
+      Availability.from(
+        OTime.createTime("2024-05-30T10:30:00").getOrElse(fail("Failed to create OTime")),
+        OTime.createTime("2024-05-30T11:30:00").getOrElse(fail("Failed to create OTime")),
+        Preference.createPreference(5).getOrElse(fail("Failed to create Preference"))
+      ),
+      Availability.from(
+        OTime.createTime("2024-05-30T14:30:00").getOrElse(fail("Failed to create OTime")),
+        OTime.createTime("2024-05-30T17:00:00").getOrElse(fail("Failed to create OTime")),
+        Preference.createPreference(5).getOrElse(fail("Failed to create Preference"))
+      )
+    )
+
+    val external1Availability = List(
+      Availability.from(
+        OTime.createTime("2024-05-30T10:00:00").getOrElse(fail("Failed to create OTime")),
+        OTime.createTime("2024-05-30T13:30:00").getOrElse(fail("Failed to create OTime")),
+        Preference.createPreference(2).getOrElse(fail("Failed to create Preference"))
+      ),
+      Availability.from(
+        OTime.createTime("2024-05-30T15:30:00").getOrElse(fail("Failed to create OTime")),
+        OTime.createTime("2024-05-30T18:00:00").getOrElse(fail("Failed to create OTime")),
+        Preference.createPreference(5).getOrElse(fail("Failed to create Preference"))
+      )
+    )
+
+    val roleList = List(
+      RoleLinkedWithResource.from(president, Name.createName("President Name").getOrElse(fail("Failed to create Name")), teacher1Availability),
+      RoleLinkedWithResource.from(advisor, Name.createName("Advisor Name").getOrElse(fail("Failed to create Name")), teacher2Availability),
+      RoleLinkedWithResource.from(supervisor, Name.createName("Supervisor Name").getOrElse(fail("Failed to create Name")), external1Availability),
+      RoleLinkedWithResource.from(coAdvisor, Name.createName("CoAdvisor Name").getOrElse(fail("Failed to create Name")), teacher1Availability)
+    )
+    PreViva.from(student, thesisTitle, roleList)
+
+  def createSimpleValidPreViva(studentName: String, title: String, presidentId: String, advisorId: String, supervisorId: String, coAdvisorId: String): PreViva =
+    val student = Name.createName(studentName).getOrElse(fail("Failed to create Name"))
+    val thesisTitle = Name.createName(title).getOrElse(fail("Failed to create Name"))
+    val president = President.from(ID.createTeacherId(presidentId).getOrElse(fail("Failed to create ID")))
+    val advisor = Advisor.from(ID.createTeacherId(advisorId).getOrElse(fail("Failed to create ID")))
+    val supervisor = Supervisor.from(ID.createExternalId(supervisorId).getOrElse(fail("Failed to create External ID")))
+    val coAdvisor = CoAdvisor.from(ID.createTeacherId(coAdvisorId).getOrElse(fail("Failed to create ID")))
+
+    val availability = List(
+      Availability.from(
+        OTime.createTime("2024-05-30T09:30:00").getOrElse(fail("Failed to create OTime")),
+        OTime.createTime("2024-05-30T12:30:00").getOrElse(fail("Failed to create OTime")),
+        Preference.createPreference(5).getOrElse(fail("Failed to create Preference"))
+      )
+    )
+
+    val roleList = List(
+      RoleLinkedWithResource.from(president, Name.createName("President Name").getOrElse(fail("Failed to create Name")), availability),
+      RoleLinkedWithResource.from(advisor, Name.createName("Advisor Name").getOrElse(fail("Failed to create Name")), availability),
+      RoleLinkedWithResource.from(supervisor, Name.createName("Supervisor Name").getOrElse(fail("Failed to create Name")), availability),
+      RoleLinkedWithResource.from(coAdvisor, Name.createName("CoAdvisor Name").getOrElse(fail("Failed to create Name")), availability)
+    )
+    PreViva.from(student, thesisTitle, roleList)
+
+  "fromCheck" should "return Right for valid start and end times" in :
     val start = OTime.createTime("2023-06-14T10:15:30").getOrElse(fail("Failed to create OTime"))
     val end = OTime.createTime("2023-06-14T12:15:30").getOrElse(fail("Failed to create OTime"))
     val preference = Preference.createPreference(3).getOrElse(fail("Failed to create Preference"))
@@ -31,7 +107,7 @@ class AvailabilityTest extends AnyFlatSpec with Matchers:
 
     result should be(Right(Availability.from(start, end, preference)))
 
-  it should "return Left for invalid start and end times" in:
+  it should "return Left for invalid start and end times" in :
     val start = OTime.createTime("2023-06-14T12:15:30").getOrElse(fail("Failed to create OTime"))
     val end = OTime.createTime("2023-06-14T10:15:30").getOrElse(fail("Failed to create OTime"))
     val preference = Preference.createPreference(3).getOrElse(fail("Failed to create Preference"))
@@ -40,7 +116,7 @@ class AvailabilityTest extends AnyFlatSpec with Matchers:
 
     result should be(Left(DomainError.WrongFormat("End time must be after start time")))
 
-  "isValid" should "return true for valid Availability" in:
+  "isValid" should "return true for valid Availability" in :
     val start = OTime.createTime("2023-06-14T10:15:30").getOrElse(fail("Failed to create OTime"))
     val end = OTime.createTime("2023-06-14T12:15:30").getOrElse(fail("Failed to create OTime"))
     val preference = Preference.createPreference(3).getOrElse(fail("Failed to create Preference"))
@@ -49,7 +125,7 @@ class AvailabilityTest extends AnyFlatSpec with Matchers:
 
     availability.isValid should be(true)
 
-  it should "return false for invalid Availability with start time after end time" in:
+  it should "return false for invalid Availability with start time after end time" in :
     val start = OTime.createTime("2023-06-14T12:15:30").getOrElse(fail("Failed to create OTime"))
     val end = OTime.createTime("2023-06-14T10:15:30").getOrElse(fail("Failed to create OTime"))
     val preference = Preference.createPreference(3).getOrElse(fail("Failed to create Preference"))
@@ -58,7 +134,7 @@ class AvailabilityTest extends AnyFlatSpec with Matchers:
 
     availability.isValid should be(false)
 
-  "intersects" should "return true for overlapping availabilities" in:
+  "intersects" should "return true for overlapping availabilities" in :
     val start1 = OTime.createTime("2023-06-14T10:15:30").getOrElse(fail("Failed to create OTime"))
     val end1 = OTime.createTime("2023-06-14T12:15:30").getOrElse(fail("Failed to create OTime"))
     val preference1 = Preference.createPreference(3).getOrElse(fail("Failed to create Preference"))
@@ -72,7 +148,7 @@ class AvailabilityTest extends AnyFlatSpec with Matchers:
 
     Availability.intersects(availability1, availability2) should be(true)
 
-  it should "return false for non-overlapping availabilities" in:
+  it should "return false for non-overlapping availabilities" in :
     val start1 = OTime.createTime("2023-06-14T10:15:30").getOrElse(fail("Failed to create OTime"))
     val end1 = OTime.createTime("2023-06-14T11:15:30").getOrElse(fail("Failed to create OTime"))
     val preference1 = Preference.createPreference(3).getOrElse(fail("Failed to create Preference"))
@@ -85,3 +161,68 @@ class AvailabilityTest extends AnyFlatSpec with Matchers:
     val availability2 = Availability.from(start2, end2, preference2)
 
     Availability.intersects(availability1, availability2) should be(false)
+
+  "chooseFirstPossibleAvailability" should "return the first available slot" in:
+    val duration = createDuration("01:00:00")
+
+    val start1 = OTime.createTime("2024-05-30T09:00:00").getOrElse(fail("Failed to create OTime"))
+    val end1 = OTime.createTime("2024-05-30T10:30:00").getOrElse(fail("Failed to create OTime"))
+    val start2 = OTime.createTime("2024-05-30T11:00:00").getOrElse(fail("Failed to create OTime"))
+    val end2 = OTime.createTime("2024-05-30T12:00:00").getOrElse(fail("Failed to create OTime"))
+
+    val availabilities = List(
+      Availability.from(start1, end1, Preference.createPreference(3).getOrElse(fail("Failed to create Preference"))),
+      Availability.from(start2, end2, Preference.createPreference(5).getOrElse(fail("Failed to create Preference")))
+    )
+
+    val usedSlots = List.empty[(HashSet[ID], Availability)]
+    val newIds = HashSet(ID.createTeacherId("T001").getOrElse(fail("Failed to create ID")))
+
+    val result = Availability.chooseFirstPossibleAvailability(availabilities, duration, usedSlots, newIds)
+
+    result._1 shouldBe defined
+    result._1.value._1 should be(start1.toLocalDateTime)
+    result._1.value._2 should be(start1.toLocalDateTime.plus(duration.toDuration))
+    result._1.value._3 should be(3)
+
+  "chooseFirstPossibleAvailabilitiesSlot" should "return the first available slot" in:
+    val duration = createDuration("01:00:00")
+
+    val start1 = OTime.createTime("2024-05-30T09:00:00").getOrElse(fail("Failed to create OTime"))
+    val end1 = OTime.createTime("2024-05-30T10:30:00").getOrElse(fail("Failed to create OTime"))
+    val start2 = OTime.createTime("2024-05-30T11:00:00").getOrElse(fail("Failed to create OTime"))
+    val end2 = OTime.createTime("2024-05-30T12:00:00").getOrElse(fail("Failed to create OTime"))
+
+    val availabilities = List(
+      Availability.from(start1, end1, Preference.createPreference(3).getOrElse(fail("Failed to create Preference"))),
+      Availability.from(start2, end2, Preference.createPreference(5).getOrElse(fail("Failed to create Preference")))
+    )
+
+    val usedSlots = List.empty[Availability]
+
+    val result = Availability.chooseFirstPossibleAvailabilitiesSlot(availabilities, duration, usedSlots)
+
+    result._1 shouldBe defined
+    result._1.value._1 should be(start1.toLocalDateTime)
+    result._1.value._2 should be(start1.toLocalDateTime.plus(duration.toDuration))
+    result._1.value._3 should be(3)
+
+  it should "return None when all availabilities are used" in:
+    val duration = createDuration("01:00:00")
+
+    val start1 = OTime.createTime("2024-05-30T09:00:00").getOrElse(fail("Failed to create OTime"))
+    val end1 = OTime.createTime("2024-05-30T10:00:00").getOrElse(fail("Failed to create OTime"))
+
+    val availabilities = List(
+      Availability.from(start1, end1, Preference.createPreference(3).getOrElse(fail("Failed to create Preference")))
+    )
+
+    val usedAvailability = Availability.from(start1, end1, Preference.createPreference(3).getOrElse(fail("Failed to create Preference")))
+
+    val usedSlots = List(usedAvailability)
+
+    val result = Availability.chooseFirstPossibleAvailabilitiesSlot(availabilities, duration, usedSlots)
+
+    result._1 shouldBe None
+
+
